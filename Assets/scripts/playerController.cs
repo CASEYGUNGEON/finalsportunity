8c21001b3ffe8d256a093560f04044a27fe0c5f4
cloudbound9 <connor@campi.cc>
2020-08-04 15:41:29 -0700

    synced player w network on spawn

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -154,5 +154,6 @@ public class playerController : PlayerBehavior
         {
             transform.position = spawn2.transform.position;
         }
+        networkObject.position = transform.position;
     }
 }
